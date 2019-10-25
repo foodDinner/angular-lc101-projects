d7b2f8d0e7ba68bc231d40d5dad14a88d7e2d6d7
foodDinner <specialdelivery@gmail.com>
2019-10-25 15:15:42 -0500

    yeah yeah

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://preview.redd.it/cn7neybsofu31.jpg?width=640&height=853&crop=smart&auto=webp&s=bec20d5f6f4761bbc5f63657e00e3a831c68506d';
+  image3 = 'https://i.redd.it/4xmnv8n91gu31.png';
 
   constructor() { }
 
--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
-
+   
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie (newTitle: string) {
+      if(!this.movies.includes(newTitle)){
+         this.movies.push(newTitle);
+      }
+      else {alert("IT'S ALREADY ON THE LIST")}
    }
 }
